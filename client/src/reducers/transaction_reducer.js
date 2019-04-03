c7d0bb8817411f31ba48dabbf3e5188fe8e429da
Divyasam <divyat2017@gmail.com>
2019-04-03 10:18:30 -0700

    Update transaction_reducer.js

--- client/src/reducers/transaction_reducer.js
+++ client/src/reducers/transaction_reducer.js
@@ -2,8 +2,6 @@ export default function(state={}, action) {
 	switch(action.type) {
 		case 'TRANSACTION_LIST':
 		  return {...state, transactions: action.payload}
-		 // case 'EMAIL_BY_ID':
-		 //  return {...state, email: action.payload}
 		default:
 		  return state;
 	}
